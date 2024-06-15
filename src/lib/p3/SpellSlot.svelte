1c47a5cd4c9438a7896338874a37a2510091ebb6
Sam Storment <sjstorment22@gmail.com>
2024-06-15 00:53:12 -0500

    Hardcode input height because there's a difference between windows and linux?

--- src/lib/p3/SpellSlot.svelte
+++ src/lib/p3/SpellSlot.svelte
@@ -1,4 +1,4 @@
 <div class="flex w-full items-end gap-1">
     <input type="checkbox" class="w-4 h-4">
-    <input type="text" class="px-1 border-b border-zinc-800 leading-none flex-1">
+    <input type="text" class="px-1 border-b border-zinc-800 leading-none flex-1 h-5">
 </div>
